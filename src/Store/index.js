f6810eb5ee35078ca1c575fb756ce9fbc1c7e9ef
Praveen Reddy <praveen.tharigonda@zmail.ril.com>
2020-11-02 18:21:08 +0530

    remove unwanted code

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,12 +1,4 @@
-// import {createStore, applyMiddleware} from 'redux';
-// import mainReducer from '../Reducers';
-// import thunk from 'redux-thunk';
-// export default function configureStore(){
-//     return createStore(
-//         mainReducer,
-//         applyMiddleware(thunk)
-//     )
-// }
+
 import { createStore as reduxCreateStore } from "redux";
 
 let store;
